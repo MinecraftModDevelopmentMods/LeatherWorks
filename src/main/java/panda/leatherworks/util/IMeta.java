66928e7e351eae17e808c8913791e7fbd935455e
Erlend Åmdal <erlend@aamdal.com>
2017-06-14 00:03:16 +0200

    Add deprecation descriptions

--- src/main/java/panda/leatherworks/util/IMeta.java
+++ src/main/java/panda/leatherworks/util/IMeta.java
@@ -6,6 +6,10 @@ import javax.annotation.Nonnull;
 
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 
+/**
+ * Item renderers could be registered explicitly like vanilla Minecraft does.
+ * Locking variant models behind this interface would then be unnecessary.
+ */
 @Deprecated
 public interface IMeta {
 
