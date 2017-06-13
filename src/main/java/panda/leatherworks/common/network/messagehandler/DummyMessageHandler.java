315bb82c1eaa111346e07216b2d1e4d2bc876b5c
Erlend Åmdal <erlend@aamdal.com>
2017-06-13 23:18:03 +0200

    Add description to dummy message handler

--- src/main/java/panda/leatherworks/common/network/messagehandler/DummyMessageHandler.java
+++ src/main/java/panda/leatherworks/common/network/messagehandler/DummyMessageHandler.java
@@ -4,6 +4,13 @@ import net.minecraftforge.fml.common.network.simpleimpl.IMessage;
 import net.minecraftforge.fml.common.network.simpleimpl.IMessageHandler;
 import net.minecraftforge.fml.common.network.simpleimpl.MessageContext;
 
+/**
+ * This dummy handler will do nothing.
+ *
+ * Forge doesn't let us register messages without a handler. This is unfortunate because it makes
+ * registering messages that should never be handled on a physical side inconvenient.
+ * Use this message handler when registering client handled messages on a dedicated server.
+ */
 public class DummyMessageHandler implements IMessageHandler<IMessage, IMessage> {
 	@Override
 	public IMessage onMessage(IMessage message, MessageContext ctx) {
