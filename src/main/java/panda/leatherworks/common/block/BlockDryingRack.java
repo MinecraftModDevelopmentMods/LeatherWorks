0335b7cb21bb2f10845e090cbb3b740931a08b51
Erlend Åmdal <erlend@aamdal.com>
2017-06-16 23:29:34 +0200

    Fix drying rack behavior

--- src/main/java/panda/leatherworks/common/block/BlockDryingRack.java
+++ src/main/java/panda/leatherworks/common/block/BlockDryingRack.java
@@ -46,12 +46,15 @@ public class BlockDryingRack extends BlockTileEntity<TileEntityDryingRack> {
 		if (!world.isRemote) {
 			TileEntityDryingRack tile = getTileEntity(world, pos);
 			IItemHandler itemHandler = tile.getCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY, side);
-			//TODO
-			if (player.inventory.addItemStackToInventory(itemHandler.extractItem(0, 1, false))) {
-				if (DryingRecipes.hasRecipe(heldItem)) {
-					player.setHeldItem(hand, itemHandler.insertItem(0, heldItem, false));
-				} else {
-					player.inventory.addItemStackToInventory(itemHandler.extractItem(0, 1, false));
+			if (itemHandler != null) {
+				// Simulate first
+				ItemStack extract = itemHandler.extractItem(0, 1, true);
+				if (extract == null) {
+					if (DryingRecipes.hasRecipe(heldItem)) {
+						player.setHeldItem(hand, itemHandler.insertItem(0, heldItem, false));
+					}
+				} else if (player.inventory.addItemStackToInventory(extract)) {
+					itemHandler.extractItem(0, 1, false);
 				}
 			}
 		}
